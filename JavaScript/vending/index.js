071f3af3fa135d125d4ff6ccb12ce0567e809bf9
hsprak9781 <hspark9781@gmail.com>
2023-09-04 15:53:23 +0900

    [JavaScript]20230904-async,await,vending

---
+++ JavaScript/vending/index.js
@@ -0,0 +1,7 @@
+import ColaGenerator from "./js/colaGenerator.js";
+
+const colaGenerator = new ColaGenerator();
+
+colaGenerator.setup();
+
+
---
+++ JavaScript/vending/js/colaGenerator.js
@@ -0,0 +1,45 @@
+class ColaGenerator {
+    constructor() {
+        this.itemList = document.querySelector('.cola-list');
+    }
+
+    async setup() {
+        const response = await this.loadData();
+        this.colaFactory(response);
+    }
+
+    async loadData() {
+
+        try{
+            const response = await fetch('./items.json');
+        
+            if(!response.ok) {
+                throw new Error('HTTP ERROR!! : ' + response.status);
+            }
+    
+            return await response.json();
+        } catch(error) {
+            console.error('콜라데이터를 로딩하는 중에 문제가 발생했습니다. : ' + error);
+        }
+    }
+
+        
+
+    colaFactory(data) {
+        const docFrag = new DocumentFragment();
+        data.forEach((el) => {
+            const item = document.createElement('li');
+            const itemTemplate = `<button type="button" class="btn-cola on">
+            <img src="images/cola-original.png" alt="" />
+            <span class="cola-name">Original_Cola</span>
+            <strong class="cola-price">1000원</strong>
+        </button>`
+            item.innerHTML = itemTemplate;
+            docFrag.append(item);
+        });
+
+        this.itemList.append(docFrag);
+    }
+}
+
+export default ColaGenerator;
