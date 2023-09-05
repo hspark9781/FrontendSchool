216497b8d76a662e41a03ec34910ca7114fd6428
hsprak9781 <hspark9781@gmail.com>
2023-09-06 08:58:50 +0900

    [JavaScript]20230906-vending

--- JavaScript/vending/js/colaGenerator.js
+++ JavaScript/vending/js/colaGenerator.js
@@ -3,37 +3,39 @@ class ColaGenerator {
         this.itemList = document.querySelector('.cola-list');
     }
 
+    // 콜라 객체를 초기화합니다.
     async setup() {
         const response = await this.loadData();
         this.colaFactory(response);
     }
 
+    // 콜라 관련 데이터를 로드합니다.
     async loadData() {
 
-        try{
+        try {
             const response = await fetch('./items.json');
-        
-            if(!response.ok) {
-                throw new Error('HTTP ERROR!! : ' + response.status);
+
+            if (!response.ok) {
+                throw new Error('HTTP ERROR!! :' + response.status);
             }
-    
             return await response.json();
-        } catch(error) {
-            console.error('콜라데이터를 로딩하는 중에 문제가 발생했습니다. : ' + error);
+        } catch (error) {
+            console.error('콜라데이터를 로딩하는중에 문제가 발생했습니다. :' + error);
         }
     }
 
-        
-
+    // 콜라의 템플릿 코드입니다.
     colaFactory(data) {
         const docFrag = new DocumentFragment();
         data.forEach((el) => {
             const item = document.createElement('li');
-            const itemTemplate = `<button type="button" class="btn-cola on">
-            <img src="images/cola-original.png" alt="" />
-            <span class="cola-name">Original_Cola</span>
-            <strong class="cola-price">1000원</strong>
-        </button>`
+            const itemTemplate = `
+            <button type="button" class="btn-cola" data-item="${el.name}" data-count="${el.count}" data-price="${el.cost}" data-img="${el.img}">
+                <img src="./images/${el.img}" alt="${el.name}" />
+                <span class="cola-name">${el.name}</span>
+                <strong class="cola-price">${el.cost}원</strong>
+            </button>
+            `;
             item.innerHTML = itemTemplate;
             docFrag.append(item);
         });
